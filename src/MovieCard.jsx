e6f067879c77fe333da5f7059898f63cd647983e
ALMAZ-09021 <almaz.khaybullin@mail.ru>
2025-06-26 15:04:25 +0300

    Added MovieCard

---
+++ src/MovieCard.jsx
@@ -0,0 +1,10 @@
+export function MovieCard({image,rating}){
+    return (
+        <div>
+            <div>
+                IMDb rating: {rating}
+            </div>
+            <img src={image} alt=""/>
+        </div>
+    )
+}
