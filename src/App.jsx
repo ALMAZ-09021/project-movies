92f7dc4f2a1e5f95ef060f86fe6a08277f59b74e
ALMAZ-09021 <almaz.khaybullin@mail.ru>
2025-06-26 16:06:57 +0300

    Added styles via tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import {MovieCard} from "./MovieCard.jsx";
+import {MovieCard} from "./MovieCard";
+
 
 function App() {
 
   return (
     <>
-        <MovieCard/>
+        <div className="min-h-screen w-full bg-black text-white px-6 py-5">
+            <header className="mb-10 flex items-center">
+                <img
+                    src="/netflix.png"
+                    alt="Netflix"
+                    className="h-8 w-auto"
+                />
+            </header>
+            <main className="flex gap-6">
+        <MovieCard image='/f1_poster.jpg' rating={8.8}/>
+            </main>
+        </div>
     </>
   )
 }
