3a954525cb77540338026bacb572dad9193aa433
ALMAZ-09021 <almaz.khaybullin@mail.ru>
2025-06-27 15:43:56 +0300

    Added search via delay

---
+++ src/movies.data.js
@@ -0,0 +1,13 @@
+export const MOVIES=[
+    {
+        name:'Formula 1',
+        image:'/f1_poster.jpg',
+        rating:'8.8'
+    },
+
+    {
+        name:'Penguins',
+        image:'/penguins.jpg',
+        rating:'9.0'
+    },
+]
